75a3179e6665e20e4fb72a8fadbcde3443178992
lenaindelaforetmagique <morin_xavier@hotmail.fr>
2018-04-09 12:17:12 +0200

    "aiguilles camembert"

--- p5/exo01/sketch.js
+++ p5/exo01/sketch.js
@@ -20,26 +20,37 @@ function draw() {
   // noFill();
   // ellipse(200, 200, 300, 300);
 
-  strokeWeight(2);
-  stroke(255, 100, 150);
-  noFill();
-  let ends = map(sc, 0, 60, 0, 360);
-  arc(0, 0, 300, 300, 0, ends); //, CHORD); //OPEN); //PIE);
-  line(0, 0, 140 * cos(sc * 6), 140 * sin(sc * 6));
 
-  strokeWeight(4);
-  stroke(100, 255, 150);
-  let mn2 = mn + sc / 60;
-  let endm = map(mn2, 0, 60, 0, 360);
-  arc(0, 0, 310, 310, 0, endm);
-  line(0, 0, 120 * cos(mn2 * 6), 120 * sin(mn2 * 6));
 
   strokeWeight(8);
   stroke(100, 150, 255);
+  noFill();
   let hr2 = hr % -12 + mn / 60 + sc / 3600;
   let endh = map(hr2, 0, 12, 0, 360);
-  arc(0, 0, 320, 320, 0, endh);
-  line(0, 0, 60 * cos(hr2 * 30), 60 * sin(hr2 * 30));
+  let r = 100;
+  arc(0, 0, 2 * r, 2 * r, 0, endh);
+  line(0, 0, r * 1, r * 0);
+  line(0, 0, r * cos(hr2 * 30), r * sin(hr2 * 30));
+
+
+  strokeWeight(4);
+  stroke(100, 255, 150);
+  r = 120;
+  let mn2 = mn + sc / 60;
+  let endm = map(mn2, 0, 60, 0, 360);
+  arc(0, 0, 2 * r, 2 * r, 0, endm);
+  line(0, 0, r * 1, r * 0);
+  line(0, 0, r * cos(mn2 * 6), r * sin(mn2 * 6));
+
+  r = 140;
+  strokeWeight(2);
+  stroke(255, 100, 150);
+
+  let ends = map(sc, 0, 60, 0, 360);
+  arc(0, 0, 2 * r, 2 * r, 0, ends); //, CHORD); //OPEN); //PIE);
+  line(0, 0, r * 1, r * 0);
+  line(0, 0, r * cos(sc * 6), r * sin(sc * 6));
+
 
   // fill(255);
   // noStroke();
