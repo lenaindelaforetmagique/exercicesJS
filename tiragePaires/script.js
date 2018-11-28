a2922ff151d76c8573bd84c1ddbe405311f27b8c
lenaindelaforetmagique <morin_xavier@hotmail.fr>
2018-11-28 14:19:21 +0100

    ajout query "data" + organisation

--- tiragePaires/script.js
+++ tiragePaires/script.js
@@ -1,3 +1,5 @@
+// General functions
+////////////////////
 var removeDOMChildren = function(dom) {
   //removes all children of dom
   while (dom.firstChild) {
@@ -5,30 +7,57 @@ var removeDOMChildren = function(dom) {
   };
 };
 
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return false;
+}
 
+
+// Interactions with scriptNoel
+///////////////////////////////
 var showResult = function(hat) {
   var domParent = document.getElementById('resultat');
   removeDOMChildren(domParent);
-  hat.resultLines().forEach(function(v) {
+  var lines = hat.resultLines();
+  if (lines.length > 0) {
+    lines.forEach(function(v) {
+      var dom = document.createElement('p');
+      dom.innerHTML = v;
+      domParent.appendChild(dom);
+    });
+  } else {
     var dom = document.createElement('p');
-    dom.innerHTML = v;
+    dom.innerHTML = " --";
     domParent.appendChild(dom);
-  });
+  }
 };
 
 var showPersons = function(hat) {
   var domParent = document.getElementById('personnes');
   removeDOMChildren(domParent);
-  hat.persons.forEach(function(v) {
+  if (hat.persons.length > 0) {
+    hat.persons.forEach(function(v) {
+      var dom = document.createElement('p');
+      dom.innerHTML = v.toString();
+      domParent.appendChild(dom);
+    });
+  } else {
     var dom = document.createElement('p');
-    dom.innerHTML = v.toString();
+    dom.innerHTML = " -- Ajoutez des personnes en cliquant sur le bouton --";
     domParent.appendChild(dom);
-  });
+  }
 }
 
 var showLists = function(hat) {
   var doms = ["list1", "list2"];
-  for (i = 0; i < 2; i++) {
+  for (var i = 0; i < 2; i++) {
     var domParent = document.getElementById(doms[i]);
     var j = domParent.selectedIndex;
     removeDOMChildren(domParent);
@@ -41,36 +70,9 @@ var showLists = function(hat) {
   }
 }
 
-var showHat = function(hat) {
-  showResult(hat);
-  showPersons(hat);
-  showLists(hat);
-
-  saveHistory();
-}
-
-export var tirage = function() {
-  hat.findPerm()
-  showHat(hat);
-}
-
-export var init = function() {
-  hat = new scriptNoel.Hat();
-  _history = [];
-  showHat(hat);
-}
-
 var addPerson = function(name) {
   hat.addPerson(name);
   _history.push(["addP", name]);
-}
-
-export var ajoutPersonne = function() {
-  var result = window.prompt("Saisissez le nom", "");
-  if (result != "" && result != null) {
-    addPerson(result);
-    showHat(hat);
-  }
 }
 
 var addLink = function(i, j) {
@@ -78,7 +80,57 @@ var addLink = function(i, j) {
   _history.push(["addL", i, j]);
 }
 
-export var ajoutLien = function() {
+
+// History functions
+////////////////////
+var saveHistory = function() {
+  localStorage["PaireNoelHistory"] = JSON.stringify(_history);
+  console.log(localStorage["PaireNoelHistory"]);
+}
+
+var getSavedHistory = function() {
+  var hist = [];
+  var stored = localStorage["PaireNoelHistory"];
+  if (stored) {
+    hist = JSON.parse(stored);
+  }
+  return hist;
+}
+
+var playHistory = function(hist) {
+  _history = []; // reset
+  for (var i = 0; i < hist.length; i++) {
+    if (hist[i][0] == "addP") {
+      addPerson(hist[i][1]);
+    } else if (hist[i][0] == "addL") {
+      addLink(hist[i][1], hist[i][2]);
+    }
+  }
+}
+
+// Button functions
+///////////////////
+export var tirageBtn = function() {
+  hat.findPerm()
+  showHat(hat);
+}
+
+export var initBtn = function() {
+  hat = new scriptNoel.Hat();
+  _history = [];
+  saveHistory();
+  showHat(hat);
+}
+
+export var ajoutPersonneBtn = function() {
+  var result = window.prompt("Saisissez le nom", "");
+  if (result != "" && result != null) {
+    addPerson(result);
+    showHat(hat);
+  }
+}
+
+export var ajoutLienBtn = function() {
   var doms = ["list1", "list2"];
   var i = document.getElementById("list1").selectedIndex;
   var j = document.getElementById("list2").selectedIndex;
@@ -88,46 +140,36 @@ export var ajoutLien = function() {
   showHat(hat);
 }
 
-var saveHistory = function() {
-  localStorage["PaireNoelHistory"] = JSON.stringify(_history);
+var showHat = function(hat) {
+  showResult(hat);
+  showPersons(hat);
+  showLists(hat);
+  saveHistory();
 }
 
-var playSavedHistory = function() {
-  _history = [];
-  var stored = localStorage["PaireNoelHistory"];
-  // console.log("playHist", hist, typeof(hist), hist.length);
-  var res = false;
-  if (stored) {
-    var hist = JSON.parse(stored);
-    if (hist.length > 0) {
-      for (var i = 0; i < hist.length; i++) {
-        if (hist[i][0] == "addP") {
-          addPerson(hist[i][1]);
-        } else if (hist[i][0] == "addL") {
-          addLink(hist[i][1], hist[i][2]);
-        }
-      };
-      res = true;
-    }
-  }
-  return res;
-}
 
-// -----------------
+// Run at start
+///////////////
 var hat = new scriptNoel.Hat();
 var _history = [];
-
-if (!playSavedHistory()) {
-  var names = ["Lorraine", "Antoine", "Stéphane", "Xavier",
-    "Jean-Baptiste", "Pierre-Hugues", "Elsa", "Emmanuelle"
-  ];
-  for (var i = 0; i < names.length; i++) {
-    addPerson(names[i]);
+var hist = [];
+let dataFileName = getQueryVariable("data");
+if (!dataFileName) {
+  hist = getSavedHistory();
+  playHistory(hist);
+  showHat(hat);
+} else {
+  // chargeJSON
+  var request = new XMLHttpRequest();
+  request.onload = function() {
+    hist = request.response;
+    if (!hist) {
+      hist = [];
+    }
+    playHistory(hist);
+    showHat(hat);
   }
-  for (var i = 0; i < 3; i++) {
-    addLink(i, i + 5);
-    addLink(i + 5, i);
-  }
+  request.open('GET', dataFileName);
+  request.responseType = 'json';
+  request.send();
 }
-
-showHat(hat);
